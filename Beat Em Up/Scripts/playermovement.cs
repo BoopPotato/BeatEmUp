f67467cb9a3e18f1b1701de758239e6951a2ef0d
BoopPotato <148252519+BoopPotato@users.noreply.github.com>
2023-11-15 21:37:12 -0500

    Second Push
    Rudimentary movement implemented. Made it to where C# Scripts work.

--- Beat Em Up/Scripts/playermovement.cs
+++ Beat Em Up/Scripts/playermovement.cs
@@ -1,10 +1,15 @@
 using Godot;
 using System;
 
-public partial class playermovement : Area2D
+public partial class PlayerMovement : CharacterBody3D
 {
 	[Export]
-	private int _playerSpeed { get; set; } = 400;
+	private int _playerSpeed { get; set; } = 10;
+	
+	[Export]
+	private int _fallAcceleration { get; set; } = 75;
+	
+	private Vector3 _targetVelocity = Vector3.Zero;
 	
 	
 	// Called when the node enters the scene tree for the first time.
@@ -14,7 +19,25 @@ public partial class playermovement : Area2D
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
+		Vector3 movementDirection = Vector3.Zero;
+
+
+		if (Input.IsActionPressed("move_right")) { movementDirection.X += 1.0f; }
+		if (Input.IsActionPressed("move_left")) { movementDirection.X -= 1.0f; }
+		if (Input.IsActionPressed("move_up")) { movementDirection.Z -= 1.0f; }
+		if (Input.IsActionPressed("move_down")) { movementDirection.Z += 1.0f; }
+
+		if (movementDirection != Vector3.Zero)
+		{
+			movementDirection = movementDirection.Normalized();
+		}
+
+		_targetVelocity.X = movementDirection.X * _playerSpeed;
+		_targetVelocity.Z = movementDirection.Z * _playerSpeed;
+
+		Velocity = _targetVelocity;
+		MoveAndSlide();
 	}
 }
